294db2bf6477e93ce9782f6f47bea46556d8ea98
LaptopWanry <wanrylaptopfc@gmail.com>
2026-05-11 11:12:51 +0700

    feat: add Google Tag Manager (GTM-MWMLRPDR)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import Header from "@/components/Header";
 import FloatingContact from "@/components/FloatingContact";
+
+const GTM_ID = "GTM-MWMLRPDR";
 
 export const metadata: Metadata = {
   title: "Thu Mua Laptop Cũ Giá Cao Tại TP.HCM | LaptopWanry",
@@ -23,7 +26,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="vi" className="antialiased" suppressHydrationWarning>
+      {/* Google Tag Manager */}
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`,
+        }}
+      />
+
       <body className="min-h-dvh flex flex-col" suppressHydrationWarning>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <Header />
         <main className="flex-1">{children}</main>
         <FloatingContact />
